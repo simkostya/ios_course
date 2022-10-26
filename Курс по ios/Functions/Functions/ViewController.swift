395275585336a43c7f7149d75790aa66e396f8bc
Костя Симончик <simonchik2210@gmail.com>
2022-10-26 21:56:11 +0200

    Block_2_Functions

---
+++ Курс по ios/Functions/Functions/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Functions
+//
+//  Created by Костя Симончик on 26.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*
+         my code
+         */
+    }
+
+
+}
+
