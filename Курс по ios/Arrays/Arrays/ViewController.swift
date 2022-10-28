710fec2713238a98753891fac8e2cbdbf5e5ada7
Костя Симончик <simonchik2210@gmail.com>
2022-10-28 14:10:49 +0200

    Arrays

---
+++ Курс по ios/Arrays/Arrays/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Arrays
+//
+//  Created by Костя Симончик on 28.10.22.
+//
+
+/*
+ Написать функцию, которая на вход принимает масив картежей типа (Int, String), возводит Int в квадрат, фильтрует только четные Int и упорядочивает по строкам по возрастанию
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
+
