4c4dd29f2cac2f6404e9c9e43c3455b35f1fb3ce
Костя Симончик <simonchik2210@gmail.com>
2022-10-28 20:23:28 +0200

    Arrays homework completed

--- Курс по ios/Arrays/Arrays/ViewController.swift
+++ Курс по ios/Arrays/Arrays/ViewController.swift
@@ -12,11 +12,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        let myArr = [(2, "Kostya"), (3, "Evialina"), (4, "Den"), (5, "Anya"), (6, "Margarita"), (7, "Frank"), (8, "Elizavetta"), (9, "Andrey"), (10, "Jo")]
+        print(mySortedArray(myArray: myArr))
+    }
+    
+    func mySortedArray(myArray: [(Int, String)]) -> [(Int, String)] {
+        let result : [(Int, String)] = myArray.map { ($0.0 * $0.0, $0.1) }
+            .filter { $0.0 % 2 == 0 }
+            .sorted { $0.1.count < $1.1.count }
+        return result
     }
 }
-
